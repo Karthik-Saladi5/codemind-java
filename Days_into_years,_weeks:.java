10bc796dc110d75bdcacf29980c1a029790fb893
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2024-02-01 22:18:21 +0530

    import java.util.*; public class demo{     public static void main(String args[]){       Scanner sc=new Scanner(System.in);       int d=sc.nextInt();       System.out.println(d/365);       System.out.println((d%365)/7);     } }

---
+++ Days_into_years,_weeks:.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class demo{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      int d=sc.nextInt();
+      System.out.println(d/365);
+      System.out.println((d%365)/7);
+    }
+}
