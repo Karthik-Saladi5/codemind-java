7ca43812f5560330fce19552a200319970388ab2
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2024-02-01 22:08:06 +0530

    import java.util.*; public class demo{     public static void main(String args[]){       Scanner sc=new Scanner(System.in);       int x=sc.nextInt(), a=sc.nextInt(), c=sc.nextInt();       System.out.println((3*x)-a-c);     } }

---
+++ Average_weight.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class demo{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      int x=sc.nextInt(), a=sc.nextInt(), c=sc.nextInt();
+      System.out.println((3*x)-a-c);
+    }
+}
