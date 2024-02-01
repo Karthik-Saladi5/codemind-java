f15d3f15823a98244396627a69f50b85252e836e
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2024-02-01 22:11:57 +0530

    import java.util.*; public class demo{     public static void main(String args[]){       Scanner sc=new Scanner(System.in);       int a=sc.nextInt(), b=sc.nextInt();       System.out.println("Sum:"+(a+b));       System.out.println("Difference:"+(a-b));       System.out.println("Product:"+(a*b));       System.out.println("Quotient:"+(a/b));       System.out.println("Remainder:"+(a%b));     } }

---
+++ Arithmetic_operations.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class demo{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt(), b=sc.nextInt();
+      System.out.println("Sum:"+(a+b));
+      System.out.println("Difference:"+(a-b));
+      System.out.println("Product:"+(a*b));
+      System.out.println("Quotient:"+(a/b));
+      System.out.println("Remainder:"+(a%b));
+    }
+}
