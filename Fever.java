0e79baf603bd9d72b199d6825f709a7cad94d8d1
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2024-02-03 00:14:39 +0530

    import java.util.*; public class demo{     public static void main(String args[]){       Scanner sc=new Scanner(System.in);       int x=sc.nextInt();       System.out.println(x>98 ? "YES": "NO");     } }

---
+++ Fever.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class demo{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      int x=sc.nextInt();
+      System.out.println(x>98 ? "YES": "NO");
+    }
+}
