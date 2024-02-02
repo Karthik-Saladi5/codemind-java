8a8e2648aaf1790d982d7e61f92cf34006223598
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2024-02-02 11:39:48 +0530

    import java.util.*; public class demo{     public static void main(String args[]){       Scanner sc=new Scanner(System.in);       double c=sc.nextDouble();       System.out.printf("%.2f",32+c*9/5);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class demo{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      double c=sc.nextDouble();
+      System.out.printf("%.2f",32+c*9/5);
+    }
+}
