895825178b6f078f35cb9f8a61f4dfc384f6d767
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2024-02-02 23:02:15 +0530

    import java.util.*; public class demo{     public static void main(String args[]){       Scanner sc=new Scanner(System.in);       int t=sc.nextInt(), s=sc.nextInt(), b=sc.nextInt();       System.out.println(1024*t*s*b);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class demo{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      int t=sc.nextInt(), s=sc.nextInt(), b=sc.nextInt();
+      System.out.println(1024*t*s*b);
+    }
+}
