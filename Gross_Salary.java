478aa5162cb664d73542c7a521547100edbf6bc3
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2024-02-03 00:33:39 +0530

    import java.util.*; public class demo{     public static void main(String args[]){       Scanner sc=new Scanner(System.in);       double b=sc.nextDouble();       double g;       if (b<=10000)     {         g=b+(b*0.8)+(b*0.2);     }     else if(b<=20000)     {         g=b+(b*0.9)+(b*0.25);     }     // else if (b>20000)     else     {         g=b+(b*0.95)+(b*0.3);     }       System.out.printf("%.2f",g);     } }

---
+++ Gross_Salary.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class demo{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      double b=sc.nextDouble();
+      double g;
+      if (b<=10000)
+    {
+        g=b+(b*0.8)+(b*0.2);
+    }
+    else if(b<=20000)
+    {
+        g=b+(b*0.9)+(b*0.25);
+    }
+    // else if (b>20000)
+    else
+    {
+        g=b+(b*0.95)+(b*0.3);
+    }
+      System.out.printf("%.2f",g);
+    }
+}
