743d4f484e016cd5ba14ea3063169ecca9b5dd13
Karthik-Saladi5 <119729884+Karthik-Saladi5@users.noreply.github.com>
2024-02-08 12:10:23 +0530

    import java.util.*; public class demo{     public static void main(String args[]){       Scanner sc=new Scanner(System.in);       int x=sc.nextInt();       int y=sc.nextInt();
          System.out.println((x>0 && x%2==0) || (x==0 && y%2==0) ? "YES" : "NO");
        }
    }

---
+++ Can_share_equally?.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class demo{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      int x=sc.nextInt();
+      int y=sc.nextInt();
+      
+      System.out.println((x>0 && x%2==0) || (x==0 && y%2==0) ? "YES" : "NO");
+    }
+}
